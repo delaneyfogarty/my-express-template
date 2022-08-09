7563967d12204a7dc1941645cdeb6e4fc974aab9
Delaney Fogarty <delaney.fk@gmail.com>
2022-08-09 09:54:02 -0700

    created some util functions for testing purposes that set up the database, end the connection, and signup a user using credentials

---
+++ tests/utils.js
@@ -0,0 +1,31 @@
+const pool = require('../lib/utils/pool');
+const { readFileSync } = require('node:fs');
+const sql = readFileSync('../setup.sql', 'utf-8');
+const request = require('supertest');
+const app = require('../lib/app');
+
+function setupDb() {
+  return pool.query(sql);
+}
+
+function closeAll() {
+  return pool.end();
+}
+
+afterAll(closeAll);
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '1234567',
+};
+
+async function signUpUser(credentials = mockUser) {
+  const agent = request.agent(app);
+  const res = await agent.post('/api/v1/auth/signup').send(credentials);
+  return { agent, user: res.body, res, credentials };
+}
+
+module.exports = {
+  setupDb,
+  signUpUser,
+};
