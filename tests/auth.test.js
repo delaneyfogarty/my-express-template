9639dfe02911ccba11624ee881aef5aa34e4ec45
Delaney Fogarty <delaney.fk@gmail.com>
2022-08-09 14:32:12 -0700

    successfully signing in a user with signin test passing

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,6 +1,6 @@
 const { setupDb, signUpUser } = require('./utils');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('/api/v1/auth', () => {
   beforeEach(setupDb);
@@ -9,6 +9,21 @@ describe('/api/v1/auth', () => {
     const { agent, user, credentials } = await signUpUser();
     console.log('user', user);
     expect(user).toEqual({
+      id: expect.any(String),
+      email: credentials.email,
+    });
+
+    const { statusCode } = await agent.get('/api/v1/auth/verify');
+    expect(statusCode).toBe(200);
+  });
+
+  it('/signin', async () => {
+    const { credentials } = await signUpUser();
+
+    const agent = request.agent(app);
+    const res = await agent.post('/api/v1/auth/signin').send(credentials);
+
+    expect(res.body).toEqual({
       id: expect.any(String),
       email: credentials.email,
     });
